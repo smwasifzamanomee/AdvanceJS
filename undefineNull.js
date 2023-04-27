78dad064197f939b4d0fd4b646013265708b1d2d
smwasifzamanomee <wasifbdjsr@gmail.com>
2023-04-28 00:20:09 +0600

    undefine

---
+++ undefineNull.js
@@ -0,0 +1,26 @@
+let age 
+console.log(age)
+
+let age1 = undefined
+console.log(age1)
+
+let age2 = [1,2,4]
+console.log(age2[11])
+
+function add(num1, num2){
+    console.log(num1 + num2)
+    return
+}
+const result1 = add(12, 13)
+console.log(result1)
+
+function add(num1, num2){
+    console.log(num1, num2)
+}
+const result = add(12)
+console.log(result)
+
+const premik = {name: "smart dude", phone: 123456}
+console.log(premik.gf)
+
+
