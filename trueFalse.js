3af2a4e6c9ba68189a2328a300fa1245d85e66f6
smwasifzamanomee <wasifbdjsr@gmail.com>
2023-04-28 00:06:21 +0600

    truty and falsey

---
+++ trueFalse.js
@@ -0,0 +1,20 @@
+//Falsey
+// 0
+// ""
+// undefined
+// null
+// NaN
+// false
+
+//Truthy
+// '0', ' ', [], {}
+
+
+let name = 0
+
+if(name){
+    console.log("Condition is true")
+}
+else{
+    console.log("Condition is false")
+}
